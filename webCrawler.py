674353b136c01ed9aee853d62c9cbd44c26a9337
Craig Beaman <Craig.Beaman@unb.ca>
2020-07-21 22:14:28 -0300

    Update webCrawler.py
    added more options to be changed

--- webCrawler.py
+++ webCrawler.py
@@ -2,14 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def job_spider(num_days_ago_posted):
-    url = 'https://ca.indeed.com/jobs?q=software%20developer&l=Moncton,%20NB&fromage=' + str(num_days_ago_posted)
+def job_spider(search_term, province, city, num_days_ago_posted):
+    url = 'https://ca.indeed.com/jobs?q=' + search_term + '&l=' + city + ',%20' + \
+          province + '&fromage=' + str(num_days_ago_posted)
     src_code = requests.get(url)
     plain_text = src_code.text
     soup = BeautifulSoup(plain_text, 'html.parser')
 
     for jobAd in soup.findAll('div', {'class': 'jobsearch-SerpJobCard'}):
-
         # removes "new" jobs from showing new in the job title
         for span in jobAd.findAll('span', {'class': 'new'}):
             span.decompose()
@@ -22,4 +22,9 @@ def job_spider(num_days_ago_posted):
               '\n' + job_date_posted_tag.text.rstrip("\n"))
 
 
-job_spider(21)
+job_search_term = 'software developer'.replace(' ', '%20')
+job_province = 'NB'
+job_city = 'Fredericton'.replace(' ', '%20')
+num_days_posted = 3
+
+job_spider(job_search_term, job_province, job_city, num_days_posted)
